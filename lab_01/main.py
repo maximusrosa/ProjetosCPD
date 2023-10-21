7c6aced7742e858ec6bf0a98862b55726e60b1b7
Maximus Rosa <maxborgesrosa@gmail.com>
2023-10-21 02:36:59 -0300

    organizando as coisas

--- lab_01/main.py
+++ lab_01/main.py
@@ -3,8 +3,10 @@ import lab1_lib
 
 
 def main():
-    with open("numeros.csv", "r") as file:
-        lista_x = [int(x) for x in file.read().split(",")]
+    with open(sys.argv[1] + ".csv", "r") as file:
+        list_x = [int(x) for x in file.read().split(",")]
+        lab1_lib.shell_sort(list_x)
+        print(list_x)
 
 
 main()
