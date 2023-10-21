654208ce24714326bffd4a074a945426a43a8731
Maximus Rosa <maxborgesrosa@gmail.com>
2023-10-21 02:36:59 -0300

    organizando as coisas

--- lab_01/lab1_lib.py
+++ lab_01/lab1_lib.py
@@ -1,36 +1,36 @@
 # Material usado como referência: https://panda.ime.usp.br/panda/static/pythonds_pt/05-OrdenacaoBusca/OShellSort.html
 
 def main():
-    lista_x = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-    shell_sort(lista_x)
-    print(lista_x)
+    list_x = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    shell_sort(list_x)
+    print(list_x)
 
 
-def shell_sort(lista_x):
-    sublistcount = len(lista_x) // 2
+def shell_sort(list_x):
+    sublistcount = len(list_x) // 2
 
     while sublistcount > 0:
 
         for startposition in range(sublistcount):
-            gap_insertion_sort(lista_x, startposition, sublistcount)
+            gap_insertion_sort(list_x, startposition, sublistcount)
 
         print("After increments of size", sublistcount,
-              "The list is", lista_x)
+              "the list is", list_x)
 
         sublistcount = sublistcount // 2
 
 
-def gap_insertion_sort(lista_x, start, gap):
-    for i in range(start + gap, len(lista_x), gap):
+def gap_insertion_sort(list_x, start, gap):
+    for i in range(start + gap, len(list_x), gap):
 
-        currentvalue = lista_x[i]
+        currentvalue = list_x[i]
         position = i
 
-        while position >= gap and lista_x[position - gap] > currentvalue:
-            lista_x[position] = lista_x[position - gap]
+        while position >= gap and list_x[position - gap] > currentvalue:
+            list_x[position] = list_x[position - gap]
             position = position - gap
 
-        lista_x[position] = currentvalue
+        list_x[position] = currentvalue
 
 
 if __name__ == '__main__':
--- lab_01/main.py
+++ lab_01/main.py
@@ -4,7 +4,12 @@ import lab1_lib
 
 def main():
     with open("numeros.csv", "r") as file:
-        lista_x = [int(x) for x in file.read().split(",")]
+        # leia os números separados por vírgula do arquivo e coloque-os em uma lista
+        list_x = [int(x) for x in file.read().split(";")]
+
+
+        lab1_lib.shell_sort(list_x)
+        print(list_x)
 
 
 main()
