c9ee3fb7e0bac1b98b8069a72ec833a9ff2cda53
Maximus Rosa <maxborgesrosa@gmail.com>
2023-12-08 01:09:01 -0300

    base pronta
    temos uma base bem sólida, mas falta ler / escrever arquivos e redimensionar tabela quando necessário
    
    Co-Authored-By: Thiaguera <53987371+thiagovitoo@users.noreply.github.com>

--- lab_04/hash_table.py
+++ lab_04/hash_table.py
@@ -1,55 +1,68 @@
+from collections import namedtuple
+import csv
+
+ParChaveValor = namedtuple('ParChaveValor', ['chave', 'valor'])
+
 class HashTable:
     def __init__(self, size):
         self.size = size
         self.table = [[] for _ in range(size)]
 
     def _hash(self, key):
-        return ord(key[0]) % self.size
+        return ord(key[0]) % self.size            #Ex: 'Thiago' -> 'T' -> 84 % 10 = 4
 
-    def set(self, key, value):
+    def insert(self, key, value):
         hash_index = self._hash(key)
-        for kvp in self.table[hash_index]:
-            if kvp[0] == key:
-                kvp[1] = value
+        for par in self.table[hash_index]:
+            if par.chave == key:
+                par.valor = value
                 return
 
-        self.table[hash_index].append([key, value])
+        self.table[hash_index].append(ParChaveValor(key, value))
 
     def get(self, key):
         hash_index = self._hash(key)
-        for kvp in self.table[hash_index]:
-            if kvp[0] == key:
-                return kvp[1]
+        for par in self.table[hash_index]:
+            if par.chave == key:
+                return par.valor
 
         raise KeyError(f'Key {key} not found')
 
     def remove(self, key):
         hash_index = self._hash(key)
-        for i, kvp in enumerate(self.table[hash_index]):
-            if kvp[0] == key:
+        for i, par in enumerate(self.table[hash_index]):
+            if par.chave == key:
                 self.table[hash_index].pop(i)
                 return
 
         raise KeyError(f'Key {key} not found')
+    
+    def __str__(self):
+        output = ""
+        for i, par_list in enumerate(self.table):
+            output += f"{i}: "
+            if par_list:
+                output += ", ".join([f"({par.chave}, {par.valor})" for par in par_list])
+            output += "\n"
+        return output 
 
 
 def main():
-    # Create a hash table of size 10
     hash_table = HashTable(10)
 
-    # Set some values
-    hash_table.set('a1', 1)
-    hash_table.set('a2', 1)
-    hash_table.set('b', 2)
-    hash_table.set('c', 3)
-    hash_table.set('d', 4)
 
-    # Get some values
-    print(hash_table.get('a1'))
-    print(hash_table.get('a2'))
-    print(hash_table.get('b'))
-    print(hash_table.get('c'))
+    with open('players.csv', 'r') as file:
+        reader = csv.reader(file)
+        next(reader)  # Pula o cabeçalho
+        for i, row in enumerate(reader):
+            if i >= 20:
+                break
+            key = row[0]  # Supondo que a chave seja a primeira coluna
+            value = row[1:]  # E o valor seja o restante da linha
+            hash_table.insert(key, value)
 
+
+    print(hash_table)
 
 if __name__ == '__main__':
     main()
