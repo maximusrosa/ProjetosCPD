03e1b582069c0823092c5ba1bae61943980912a3
Thiaguera <53987371+thiagovitoo@users.noreply.github.com>
2023-12-16 03:01:39 -0300

    Add files via upload

--- lab_04/hash_table.py
+++ lab_04/hash_table.py
@@ -3,29 +3,26 @@ import csv
 
 ParChaveValor = namedtuple('ParChaveValor', ['id', 'chave', 'valor'])
 
+
 class HashTable:
     def __init__(self, size):
         self.size = size
-        self.count = 0  # Adiciona um contador para acompanhar o número de elementos
         self.table = [[] for _ in range(size)]
 
     def _hash(self, key):
-        return sum(ord(caractere) for caractere in key) % self.size  # Ex: 'Thiago' -> 'Thi' -> 293 % 10 = 3
+        # Ex: 'Thiago' -> 604 % 10 = 4
+        return sum(ord(caractere) for caractere in key) % self.size
 
     def insert(self, id, key, value):
-        if self.count / self.size > 0.7: # Se a taxa de ocupação é maior que 70%
-            self._resize()  # Redimensiona a tabela hash
 
-        variavel = key[0:3]  # Pega as três primeiras letras do nome como chave
+        variavel = key[0:6]  # Pega as seis primeiras letras do nome como chave
         hash_index = self._hash(variavel)
-        
-        #for par in self.table[hash_index]:                      ################################### Nem precisa disso
-         #   if par.chave == key:
-          #      print(f"Nome {key} já está na tabela")
-           #     return                                          ################################### Nem precisa disso
 
-        self.count += 1
         self.table[hash_index].append(ParChaveValor(id, key, value))
+
+        # Se a taxa de ocupação da lista encadeada é maior que 70%
+        if len(self.table[hash_index]) / self.size > 0.7:
+            self._resize()  # Redimensiona a tabela hash
 
     def _resize(self):
         self.size *= 2  # Dobra o tamanho da tabela hash
@@ -35,7 +32,6 @@ class HashTable:
                 hash_index = self._hash(par.chave[0])
                 new_table[hash_index].append(par)
         self.table = new_table
-
 
     def get(self, key):
         hash_index = self._hash(key)
@@ -59,7 +55,8 @@ class HashTable:
         for i, par_list in enumerate(self.table):
             output += f"{i}: "
             if par_list:
-                output += ", ".join([f"({par.id}, {par.chave}, {par.valor})" for par in par_list])
+                output += ", ".join(
+                    [f"({par.id}, {par.chave}, {par.valor})" for par in par_list])
             output += "\n"
         return output
 
@@ -70,9 +67,8 @@ def main():
     with open('players.csv', 'r') as file:
         reader = csv.reader(file)
         next(reader)  # Pula o cabeçalho
+        # mermão tem bruxaria aqui, se apagar esse i o código não funciona
         for i, row in enumerate(reader):
-            if i >= 100:
-                break
             id = row[0]
             key = row[1]
             value = row[2:]
