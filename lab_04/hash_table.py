b389e34cb17b3dabf749ea2a23334c8feca9ffe9
Thiaguera <53987371+thiagovitoo@users.noreply.github.com>
2023-12-15 23:33:35 -0300

    Add files via upload

--- lab_04/hash_table.py
+++ lab_04/hash_table.py
@@ -1,24 +1,41 @@
 from collections import namedtuple
 import csv
 
-ParChaveValor = namedtuple('ParChaveValor', ['chave', 'valor'])
+ParChaveValor = namedtuple('ParChaveValor', ['id', 'chave', 'valor'])
 
 class HashTable:
     def __init__(self, size):
         self.size = size
+        self.count = 0  # Adiciona um contador para acompanhar o número de elementos
         self.table = [[] for _ in range(size)]
 
     def _hash(self, key):
-        return ord(key[0]) % self.size            #Ex: 'Thiago' -> 'T' -> 84 % 10 = 4
+        return sum(ord(caractere) for caractere in key) % self.size  # Ex: 'Thiago' -> 'Thi' -> 293 % 10 = 3
 
-    def insert(self, key, value):
-        hash_index = self._hash(key)
-        for par in self.table[hash_index]:
-            if par.chave == key:
-                par.valor = value
-                return
+    def insert(self, id, key, value):
+        if self.count / self.size > 0.7: # Se a taxa de ocupação é maior que 70%
+            self._resize()  # Redimensiona a tabela hash
 
-        self.table[hash_index].append(ParChaveValor(key, value))
+        variavel = key[0:3]  # Pega as três primeiras letras do nome como chave
+        hash_index = self._hash(variavel)
+        
+        #for par in self.table[hash_index]:                      ################################### Nem precisa disso
+         #   if par.chave == key:
+          #      print(f"Nome {key} já está na tabela")
+           #     return                                          ################################### Nem precisa disso
+
+        self.count += 1
+        self.table[hash_index].append(ParChaveValor(id, key, value))
+
+    def _resize(self):
+        self.size *= 2  # Dobra o tamanho da tabela hash
+        new_table = [[] for _ in range(self.size)]
+        for bucket in self.table:
+            for par in bucket:
+                hash_index = self._hash(par.chave[0])
+                new_table[hash_index].append(par)
+        self.table = new_table
+
 
     def get(self, key):
         hash_index = self._hash(key)
@@ -36,33 +53,34 @@ class HashTable:
                 return
 
         raise KeyError(f'Key {key} not found')
-    
+
     def __str__(self):
         output = ""
         for i, par_list in enumerate(self.table):
             output += f"{i}: "
             if par_list:
-                output += ", ".join([f"({par.chave}, {par.valor})" for par in par_list])
+                output += ", ".join([f"({par.id}, {par.chave}, {par.valor})" for par in par_list])
             output += "\n"
-        return output 
+        return output
 
 
 def main():
     hash_table = HashTable(10)
 
-
     with open('players.csv', 'r') as file:
         reader = csv.reader(file)
         next(reader)  # Pula o cabeçalho
         for i, row in enumerate(reader):
-            if i >= 20:
+            if i >= 100:
                 break
-            key = row[0]  # Supondo que a chave seja a primeira coluna
-            value = row[1:]  # E o valor seja o restante da linha
-            hash_table.insert(key, value)
+            id = row[0]
+            key = row[1]
+            value = row[2:]
 
+            hash_table.insert(id, key, value)
 
     print(hash_table)
+
 
 if __name__ == '__main__':
     main()
